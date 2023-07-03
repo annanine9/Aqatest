5a922e825d83204c0fd6726d02d6690315eb5f41
annanine9 <a.deviatko@pin-up.tech>
2023-07-03 13:13:42 -0400

    new lesson1

---
+++ Lesson1.js
@@ -0,0 +1,11 @@
+// first lesson
+// #1
+let admin, name, age
+name = 'Anna';
+admin = name;
+age = 25;
+console.log (admin,age)
+
+// #2
+let nameOurPlanet = 'Earth';
+let newCurrentVisitor = 'Viva';
